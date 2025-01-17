335b47b49a80e018e43c01f78479fae07cfc56b6
Cy Jay Herrera <herreracyjay24@gmail.com>
2025-01-17 21:42:05 +0800

    temp

--- .history/tailwind.config_20241224174358.js
+++ .history/tailwind.config_20241224174358.js
@@ -7,7 +7,6 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
-        "./index.html", "./src/**/*.{html,js,jsx,ts,tsx}", "./components/**/*.{js,ts,jsx,tsx}"
     ],
 
     theme: {
