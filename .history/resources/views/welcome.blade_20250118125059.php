f511ddeca27e53258847c941f39a924ea5acd00b
Cy Jay Herrera <herreracyjay24@gmail.com>
2025-01-18 12:56:50 +0800

    fix: Slider button incorrect

--- .history/resources/views/welcome.blade_20250118125059.php
+++ .history/resources/views/welcome.blade_20250118125059.php
@@ -222,7 +222,7 @@
                     <div class="absolute top-0 left-1/2 w-96 -translate-x-1/2  z-30 h-20 ">
                         <div class="w-full h-full relative">
                             <!-- Slider controls -->
-                            <button type="button" class="absolute  top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+                            <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
                                 <span class="inline-flex items-center justify-center w-8 h-8 rounded-full bg-black opacity-50 dark:bg-gray-800/30 group-hover:bg-black/50 dark:group-hover:bg-gray-800/60  dark:group-focus:ring-gray-800/70 group-focus:outline-none">
                                     <svg class="w-3 h-3 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
@@ -230,7 +230,7 @@
                                     <span class="sr-only">Previous</span>
                                 </span>
                             </button>
-                            <button type="button" class="absolute top-0 right-0 z-30 flex items-center justify-end  h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+                            <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-end w-full h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
                                 <span class="inline-flex items-center justify-center w-8 h-8 rounded-full bg-black opacity-50 dark:bg-gray-800/30 group-hover:bg-black/50 dark:group-hover:bg-gray-800/60  dark:group-focus:ring-gray-800/70 group-focus:outline-none">
                                     <svg class="w-3 h-3 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
